f5cca7fd03c6401ca1927474b0e83741912d9893
Piyush Singh <piyushsenger205@gmail.com>
2025-12-10 12:49:52 +0530

    Listening feature

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/api/health', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5001;
-
+// port no xyz
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
